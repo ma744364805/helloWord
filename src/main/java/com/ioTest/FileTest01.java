891e40ca82e2df351e55688209b09855409c7334
ma <744364805@qq.com>
2018-03-27 17:43:29 +0800

    aaaaa

--- src/main/java/com/ioTest/FileTest01.java
+++ src/main/java/com/ioTest/FileTest01.java
@@ -7,7 +7,7 @@ public class FileTest01 {
 	public static void main(String[] args) throws IllegalAccessException, IOException {
 		// FileUtils.listDirectory(new File("E:/file"));
 		try {
-			IOUtils.printIO("E:\\file\\ad.txt");
+			IOUtils.printIO("E:\\file\\asd.txt");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
